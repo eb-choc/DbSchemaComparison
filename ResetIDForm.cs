96641c309e184d7ec847c48a7795ad31c7b79daf
Ethan.Bu <wz520a1s1,./LL19>
2020-09-29 10:25:39 +0800

    update some codes

--- ResetIDForm.cs
+++ ResetIDForm.cs
@@ -106,56 +106,78 @@ namespace DbSchemaComparison
                 MessageBox.Show("表起始值无效！");
                 return;
             }
-            CommittableTransaction trans = new CommittableTransaction(TimeSpan.FromMinutes(60));            
+            CommittableTransaction trans = new CommittableTransaction(TimeSpan.FromMinutes(120));            
             sql = "select count(*) from " + tablename;
             v = dbhelper.GetFirstValue(sql);
             int count = Convert.ToInt32(v);
             msgForm.SetText(tablename + "表找到" + count + "条数据，最大ID=" + maxId + (count > 10000 ? "，开始分批执行" : ""));
             int currentId = 0;
+            List<string> sb_updateid = new List<string>();
             int pcount = 10000;
+            int execCount = 0;
             if (count > pcount)
             {
                 while(true)
                 {
                     int currentStartNum = 0;
+                    int currentExecCount = 0;
+                    string ss1 = "";
                     sql = "select id from " + tablename + " where id > " + currentId + " order by id asc limit "+ pcount + ";";
-                    currentId = ExecResetId(dbhelper, sql, tablename, startNum, trans, out currentStartNum);
+                    currentId = ExecResetId(dbhelper, sql, tablename, startNum, trans, 
+                        out currentStartNum, out ss1, out currentExecCount);
                     if(currentId <= 0)
                     {
                         break;
                     }
                     startNum = currentStartNum;
-                    msgForm.SetText("当前执行到ID=" + currentId);
+                    execCount += currentExecCount;
+                    sb_updateid.Add(ss1);
+                    msgForm.SetText("执行到ID=" + currentId + "，剩" + (count - execCount));
+                    
                 }
             }
             else
             {
                 sql = "select id from " + tablename + " order by id asc";
                 int currStartNum;
-                ExecResetId(dbhelper, sql, tablename, startNum, trans, out currStartNum);  
+                string ss1 = "";
+                ExecResetId(dbhelper, sql, tablename, startNum, trans, out currStartNum, out ss1, out execCount);
+                sb_updateid.Add(ss1);
             }
             msgForm.SetText("执行最后的ID更新");
-            sql = "UPDATE " + tablename + " set id=id*(-1) where id < 0";
-            new MySqlDbHelper(dbhelper.ConnectionString).RunSql(sql, null, null, trans);
+            int index = 0;
+            foreach(string s in sb_updateid)
+            {
+                msgForm.SetText("执行更新第" + (index + 1) + "批");
+                new MySqlDbHelper(dbhelper.ConnectionString).RunSql(s, null, null, trans);
+                index++;
+            }
             trans.Commit();
             msgForm.SetText("执行完成");
         }
 
         private int ExecResetId(MySqlDbHelper dbhelper, string sql, string tablename, int startNum,
-            CommittableTransaction trans, out int currentStartNum)
+            CommittableTransaction trans, out int currentStartNum, out string ss1, 
+            out int currentExecCount)
         {
             int curNum = startNum;
             int lastId = 0;
+            currentExecCount = 0;
             StringBuilder sb = new StringBuilder();
             DataTable dt = dbhelper.RunDataTableSql(sql);
+            List<int> ids = new List<int>();
+            ss1 = "";
             if (dt != null && dt.Rows.Count > 0)
             {
                 foreach (DataRow row in dt.Rows)
                 {
-                    string sql_1 = "UPDATE " + tablename + " set id=" + curNum * (-1) + " where id=" + row["id"] + ";";
+                    int cid = curNum * (-1);
+                    string sql_1 = "UPDATE " + tablename + " set id=" + cid + " where id=" + row["id"] + ";";
                     sb.AppendLine(sql_1);
                     curNum++;
+                    currentExecCount++;
                     lastId = Convert.ToInt32(row["id"]);
+                    ids.Add(cid);
                 }
             }
             else
@@ -163,6 +185,7 @@ namespace DbSchemaComparison
                 currentStartNum = 0;
                 return -1;
             }
+            ss1 = "UPDATE " + tablename + " set id=id*(-1) where id in (" + string.Join(",", ids.ToArray()) + ");";
 
             new MySqlDbHelper(dbhelper.ConnectionString).RunSql(sb.ToString(), null, null, trans);            
             currentStartNum = curNum;
